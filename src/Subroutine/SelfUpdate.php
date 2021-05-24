5acc239a5e43e9f036c731099a740739e4e2b314
Gerhard Seidel <gseidel.message@googlemail.com>
2021-05-24 22:07:48 +0800

    Implement self-update function

--- src/Subroutine/SelfUpdate.php
+++ src/Subroutine/SelfUpdate.php
@@ -10,6 +10,19 @@ class SelfUpdate extends AbstractSubroutine implements SubroutineInterface
 {
     public function __invoke(): int
     {
+        $url = "https://github.com/enhavo/enhavo-cli/releases/latest/download/enhavo.phar";
+        $localFilename = realpath($_SERVER['argv'][0]) ?: $_SERVER['argv'][0];
+
+        if (is_writable($localFilename)) {
+            $this->output->writeln('Download file ...');
+            file_put_contents($localFilename, file_get_contents($url));
+            chmod($localFilename, 755);
+            $this->output->writeln('Updated!');
+        } else {
+            $this->output->writeln('File is not writeable. Can\'t update!');
+            return Command::FAILURE;
+        }
+
         return Command::SUCCESS;
     }
 }
